21d5f1edc4b3518e4dc271920a64fb1321177af5
Byeonggill Choi <impeach94@naver.com>
2019-08-07 16:15:33 +0900

    spring boot 불필요한 부분 삭제

--- backend/src/main/java/com/choi/controller/RestAPIController.java
+++ backend/src/main/java/com/choi/controller/RestAPIController.java
@@ -5,13 +5,10 @@ import com.choi.model.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -25,9 +22,7 @@ public class RestAPIController {
     @GetMapping("/users")
     List<User> getAllUsers() {
         List<User> resultUsers = userService.selectAllUsers();
-        for (User user : resultUsers) {
-            System.out.println(user);
-        }
+        logger.info("모든 사용자 조회: {}", resultUsers);
         return resultUsers;
     }
 }
